ed4013a6af8308e6878bdf9c390fc20b1a721d10
ionnuthub <raducugionut@gmail.com>
2024-04-12 20:01:45 +0300

    Changed NavBar.brand into Games List

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,13 +4,12 @@ import Navbar from "react-bootstrap/Navbar";
 function NavBar() {
   return (
     <Navbar bg="primary" data-bs-theme="dark">
-      {/* <Navbar.Brand href="#home">Home</Navbar.Brand> */}
+      <Navbar.Brand href="/">Games List</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          {/* <Nav.Link href="#home">Home</Nav.Link> */}
-          <Nav.Link href="/">Games List</Nav.Link>
-          <Nav.Link href="/slot-machine">Slot Machine</Nav.Link>
+          {/* <Nav.Link href="/">Games List</Nav.Link> */}
+          <Nav.Link href="slot-machine">Slot Machine</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
