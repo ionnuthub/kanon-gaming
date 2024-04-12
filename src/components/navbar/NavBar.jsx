527f37a93f47c6ca214da9fe7327b75823849cf5
ionnuthub <raducugionut@gmail.com>
2024-04-12 20:51:22 +0300

    Changed NavBar Link

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,15 +1,21 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
+import { Link } from "react-router-dom";
+
 function NavBar() {
   return (
     <Navbar bg="primary" data-bs-theme="dark">
-      <Navbar.Brand href="/">Games List</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">
+        Games List
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          {/* <Nav.Link href="/">Games List</Nav.Link> */}
-          <Nav.Link href="slot-machine">Slot Machine</Nav.Link>
+          {/* <Nav.Link as={Link} to="/">Games List</Nav.Link> */}
+          <Nav.Link as={Link} to="/slot-machine">
+            Slot Machine
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
